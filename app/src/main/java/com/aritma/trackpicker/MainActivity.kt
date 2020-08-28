1f7e82b47252b4d91bb29ba6fd44bff727e2c40e
Aritmacz <aritma.cz@gmail.com>
2020-08-29 00:12:13 +0200

    Defined new color scheme for more contrast and set limited initial card view size for w600dp+ devices

--- app/src/main/java/com/aritma/trackpicker/MainActivity.kt
+++ app/src/main/java/com/aritma/trackpicker/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
-import com.aritma.trackpicker.CardPicker
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -24,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         bt_confirm.setOnClickListener {
             val intent = Intent(this, CardPicker::class.java)
-            intent.putExtra("track_num", sb_track_num_slider.progress);
+            intent.putExtra("track_num", sb_track_num_slider.progress)
             startActivity(intent)
             finish()
         }
